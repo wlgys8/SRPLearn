fd4298708ceb8fffc5e3d2076e6fa6846021b846
zilch <wlgys8@gmail.com>
2021-02-10 23:45:05 +0800

    first commit

---
+++ Assets/XRendererPipeline/Runtime/XRendererPipelineAsset.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Collections;
+
+namespace SRPLearn{
+
+
+    [CreateAssetMenu(menuName = "SRPLearn/XRendererPipelineAsset")]
+    public class XRendererPipelineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new XRenderPipeline();
+        }
+    }
+
+
+    public class XRenderPipeline : RenderPipeline
+    {
+
+        private ShaderTagId _shaderTag = new ShaderTagId("ForwardBase");
+        
+
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            //遍历摄像机，进行渲染
+            foreach(var camera in cameras){
+                RenderPerCamera(context,camera);
+            }
+            //提交渲染命令
+            context.Submit();
+        }
+
+
+
+        private void RenderPerCamera(ScriptableRenderContext context,Camera camera){
+            //设置摄像机参数
+            context.SetupCameraProperties(camera);
+            //对场景进行裁剪
+            camera.TryGetCullingParameters( out var cullingParams);
+            var cullingResults = context.Cull(ref cullingParams);
+            var drawSetting = CreateDrawSettings(camera);
+            var filterSetting = new FilteringSettings(RenderQueueRange.all);
+            //绘制物体
+            context.DrawRenderers(cullingResults,ref drawSetting,ref filterSetting);
+        }
+
+        private DrawingSettings CreateDrawSettings(Camera camera){
+            var sortingSetting = new SortingSettings(camera);
+            var drawSetting = new DrawingSettings(_shaderTag,sortingSetting);
+            return drawSetting;
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# SRPLearn
+
+基于Unity可编程渲染管线造轮子。 主要目的是为了边写边学习，不考虑平台兼容和性能优化等问题。
+
+
+Unity版本: 2019.4.16
+
+注意，项目不需要用到`com.unity.render-pipelines.universal`，可以将其从包依赖中删除。
+
+# 目录
+
+- [创建渲染管线，绘制Cube](https://github.com/wlgys8/SRPLearn/wiki/Hello)
+- 支持平行光
+    - 漫反射
+    - 高光反射
+- 待补充
